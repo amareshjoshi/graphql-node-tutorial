a94b21e4bbd5cf941e9a2036a0b8c05a81be78e7
Amaresh R Joshi <joshia@msu.edu>
2021-09-30 18:55:29 -0400

    Finished section: A Simple Query

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,28 +1,56 @@
-const { ApolloServer } = require("apollo-server");
+const {ApolloServer} = require("apollo-server");
 
 //
 // this is the schema form the GraphQL db
 const typeDefs = `
     type Query {
         info: String!
-    }
+        feed: [Link!]!
+    }    
+    type Link {
+    id: ID!
+    description: String!
+    url: String!
+  }
 `;
 
+// (static) data
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+},
+    {
+        id: 'link-1',
+        url: 'www.msu.edu',
+        description: 'Michigan State University'
+    },
+]
+
 //
-// how to handle requests
+// resolvers process requests
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-  },
+    Query: {
+        info: () => `This is the API of a Hackernews Clone`,
+        // new resolver for root field (object?) feed
+        feed: () => links,
+    },
+    // resolvers for the fields in the Link object
+    Link: {
+        id: (parent) => parent.id,
+        description: (parent) => parent.description,
+        url: (parent) => parent.url,
+    }
 };
 
 //
 // create the GraphQL server with the schema and resolvers
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+    playground: true,
+    typeDefs,
+    resolvers,
 });
 
 //
 // start the server
-server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
+server.listen().then(({url}) => console.log(`Server is running on ${url}`));
