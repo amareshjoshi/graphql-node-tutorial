340c3340573ad894d6daaaf5f8bfc2cd427ac5f3
Amaresh R Joshi <joshia@msu.edu>
2021-09-30 20:53:07 -0400

    Finished section: A Simple Mutation

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,19 +1,5 @@
 const {ApolloServer} = require("apollo-server");
 
-//
-// this is the schema form the GraphQL db
-const typeDefs = `
-    type Query {
-        info: String!
-        feed: [Link!]!
-    }    
-    type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-`;
-
 // (static) data
 let links = [{
     id: 'link-0',
@@ -30,24 +16,66 @@ let links = [{
 //
 // resolvers process requests
 const resolvers = {
-    Query: {
-        info: () => `This is the API of a Hackernews Clone`,
-        // new resolver for root field (object?) feed
-        feed: () => links,
-    },
-    // resolvers for the fields in the Link object
-    Link: {
-        id: (parent) => parent.id,
-        description: (parent) => parent.description,
-        url: (parent) => parent.url,
+        Query: {
+            info: () => `This is the API of a Hackernews Clone`,
+            // new resolver for root field (object?) feed
+            feed: () => links,
+            //
+            link: (parent, args) => {
+                // return the link from the links array that matches args.id
+                for (link of links) {
+                    if (args.id == link.id) {
+                        return link;
+                    }
+                }
+            }
+        },
+        //
+        Mutation: {
+            post: (parent, args) => {
+                let idCount = links.length
+                const link = {
+                    id: `link-${idCount++}`,
+                    description: args.description,
+                    url: args.url,
+                }
+                links.push(link)
+                return link
+            },
+            // Update a link
+            updateLink: (parent, args) => {
+                // find the link with id = args.id and change its values
+                for (link of links) {
+                    if (args.id == link.id) {
+                        link.url = args.url;
+                        link.description = args.description;
+                        return link;
+                    }
+                }
+            },
+            deleteLink: (parent, args) => {
+                // find the link with id = args.id and save it
+                // remove it from the links array
+                let deleteLink = links.find(link => link.id === args.id);
+                links = links.filter(link => link.id !== args.id);
+                return deleteLink;
+            },
+        }
+        ,
     }
-};
+;
 
+//
+// i guess filesystem and path
+const fs = require('fs');
+const path = require('path');
 //
 // create the GraphQL server with the schema and resolvers
 const server = new ApolloServer({
-    playground: true,
-    typeDefs,
+    typeDefs: fs.readFileSync(
+        path.join(__dirname, 'schema.graphql'),
+        'utf8'
+    ),
     resolvers,
 });
 
